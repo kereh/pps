65142a54a1e4231e8dd12d41aedc056d1a5ebe38
kereh <kerehchannel@gmail.com>
2023-10-27 13:46:24 +0800

    update: add some components

--- src/pages/surat/daftar.tsx
+++ src/pages/surat/daftar.tsx
@@ -1,12 +1,35 @@
 import { type GetServerSideProps, GetServerSidePropsContext } from "next";
+import { getServerAuthSession } from "@/server/auth";
+import SuratList from "@/components/surat/surat-list";
 
 export default function Daftar() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <SuratList />
+    </div>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async (
   ctx: GetServerSidePropsContext,
 ) => {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/masuk",
+        permanent: false,
+      },
+    };
+  } else if (session.user.role == "ADMIN") {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
   return {
     props: {},
   };
